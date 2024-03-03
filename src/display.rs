034aea0274cb7c6ee53a655465be99d6409b6c29
Parsifa1 <li.aldric@gmail.com>
2024-03-04 06:13:26 +0800

    fix:重构，将error_handler和display合在一起

--- src/display.rs
+++ src/display.rs
@@ -1,30 +1,35 @@
+use anyhow::Result;
 pub trait SpecializedDisplay {
     fn display(self) -> Self;
 }
 
-impl SpecializedDisplay for String {
+impl SpecializedDisplay for Result<String> {
     fn display(self) -> Self {
-        self
+        self.or_else(error_handler)
     }
 }
 
-impl SpecializedDisplay for Vec<String> {
+impl SpecializedDisplay for Result<Vec<String>> {
     fn display(self) -> Self {
-        println!("你的课程列表为：");
-        self.iter().for_each(|i| print!("{} ", i));
-        self
+        self.map_err(error_handler).map(|v| {
+            println!("你的课程列表为：");
+            v.iter().for_each(|i| print!("{} ", i));
+            v
+        })
     }
 }
 
-impl SpecializedDisplay for Vec<usize> {
+impl SpecializedDisplay for Result<Vec<usize>> {
     fn display(self) -> Self {
-        println!("你选择的课程为：");
-        self.iter().for_each(|i| print!("{} ", i));
-        self
+        self.map_err(error_handler).map(|v| {
+            println!("你选择的课程为：");
+            v.iter().for_each(|i| print!("{} ", i));
+            v
+        })
     }
 }
 
-pub fn error_handler<T>(error: anyhow::Error) -> anyhow::Result<T> {
+fn error_handler<T>(error: anyhow::Error) -> T {
     println!("{}, 请检查配置文件，并重启程序", error);
     std::process::exit(1);
 }
